4e7d50c39cde307ccbb28d75e879c2cd956860ea
Seth Stenzel <seth.c.stenzel@gmail.com>
2026-08-08 22:07:08 -0500

    blog entry / edit

--- blog-release.bat
+++ blog-release.bat
@@ -1,11 +1,10 @@
-
-cd /d "%~dp0"
-git switch main
-git add .
-git commit -m "blog entry / edit"
-git push
-git switch release
-git merge main
-git push
+cd /d "%~dp0" && ^
+git switch main && ^
+git add . && ^
+git commit -m "blog entry / edit" && ^
+git push && ^
+git switch release && ^
+git merge main && ^
+git push && ^
 git switch main
 
